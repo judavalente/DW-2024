7421e960b1d7058256360879efc50258b666416d
judavalente <120988003+judavalente@users.noreply.github.com>
2024-07-02 18:20:17 -0300

    Create ex-js2.js

---
+++ exercício-js/ex-js2.js
@@ -0,0 +1,25 @@
+function calcular(num1, num2, operador) {
+  return res;
+
+  switch (operador) {
+    case '+':
+      res = num1 + num2;
+      break;
+    case '-':
+      res = num1 - num2;
+      break;
+    case '*':
+      res = num1 * num2;
+      break;
+    case '/':
+      res = num1 / num2;
+      break;
+}
+
+}
+
+const num1 = 5
+const num2 = 10
+const operador = "/"
+
+console.log(calcular(num1, num2, num3))
