f52084c020063a3587ee036faeaa5b65720cea75
judavalente <120988003+judavalente@users.noreply.github.com>
2024-07-02 18:05:28 -0300

    Create ex-js2.js

---
+++ atividade-01/ex-js2.js
@@ -0,0 +1,20 @@
+function calcular(num1, num2, operador) {
+  let result;
+
+  switch (operador) {
+    case '+':
+      res = num1 + num2;
+      break;
+    case '-':
+      res = num1 - num2;
+      break;
+    case '*':
+      res = num1 * num2;
+      break;
+    case '/':
+      res = num1 / num2;
+      break;
+}
+
+console.log(result))
+}
